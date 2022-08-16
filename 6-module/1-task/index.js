95be91c7afac971ae56e66636be06903783cf6b8
Romashova Irina <romashova.ira@mail.ru>
2022-08-16 22:24:54 +0300

    rows  Please enter the commit message for your changes. Lines starting

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,40 @@
  */
 export default class UserTable {
   constructor(rows) {
-  }
+    this.rows = rows;
+    this.elem = document.createElement("table");
+    this.makeHTML();
+}
+makeHTML() {
+    let cell = `      
+    <thead>
+    <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>` + this.rows.map(item => `
+    <tr>
+        <td>${item.name}</td>
+        <td>${item.age}</td>
+        <td>${item.salary}</td>
+        <td>${item.city}</td>
+        <td><button>X</button></td>
+    </tr>              
+            `).join("") + `</tbody>`;
+    this.elem.innerHTML = cell;
+
+    // событие на удаление
+    for (let btnlist of this.elem.querySelectorAll("button"))
+        btnlist.addEventListener("click", this);
+}
+handleEvent(event) {
+    let row = event.target.parentElement.parentElement; 
+    this.rows.splice(row.rowIndex - 1, 1); 
+    row.remove();
+}
+
 }
