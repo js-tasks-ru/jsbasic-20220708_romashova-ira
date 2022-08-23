2d703f9b9bd04e3a2bbdd754ee9fadd216013c24
Irina Romashova <novinki12.1@gmail.com>
2022-08-23 13:39:20 +0200

    create sliders

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,67 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Modal {
   constructor() {
+    this.render();
+    this.addEventListeners();
+  }
+
+  render() {
+    this.elem = createElement(`<div class="modal">
+    <!--Прозрачная подложка перекрывающая интерфейс-->
+    <div class="modal__overlay"></div>
+    <div class="modal__inner">
+      <div class="modal__header">
+        <!--Кнопка закрытия модального окна-->
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <h3 class="modal__title">
+        </h3>
+      </div>
+      <div class="modal__body">
+      </div>
+    </div>
+  </div>`);
+  }
+
+  open() {
+    document.body.classList.add("is-modal-open");
+    document.body.append(this.elem);
+    this.addEventListeners();
+  }
+
+  setTitle(titleInner) {
+    let title = this.elem.querySelector(".modal__title");
+    title.innerHTML = titleInner;
+  }
+
+  setBody(bodyInner) {
+    let body = this.elem.querySelector(".modal__body");
+    body.innerHTML = null;
+    body.append(bodyInner);
+  }
+
+  close() {
+    document.body.classList.remove("is-modal-open");
+    this.elem.remove();
+    document.removeEventListener("keydown", this.escFunc);
+    document.removeEventListener('click', this.onClick);
+  }
+
+  escFunc = (event) => {
+    if (event.code === "Escape") {
+      this.close();
+    }
+  };
+  onClick = ({ target }) => {
+    if (target.closest(".modal__close")) {
+      this.close();
+    }
+  };
+
+  addEventListeners() {
+    document.addEventListener("click", this.onClick);
+    document.addEventListener("keydown", this.escFunc);
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,62 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.elem = document.createElement("div");
+    this.render();
+    this.addEventListeners();
+  }
+  render() {
+    this.elem.innerHTML = `
+    <div class="slider">
+    <!--Ползунок слайдера с активным значением-->
+    <div class="slider__thumb" style="left: 0%;">
+      <span class="slider__value">${this.value}</span>
+    </div>
+    <!--Заполненная часть слайдера-->
+    <div class="slider__progress" style="width: 0%;"></div>
+    <!--Шаги слайдера-->
+    <div class="slider__steps">
+    </div>
+  </div>
+    `;
+    this.sliderSteps();
+  }
+  sliderSteps() {
+    let sliderSteps = this.elem.querySelector(".slider__steps");
+    let sliders = [];
+    for (let i = 0; i < this.steps; i++) {
+      if (i == 0) {
+        sliders.push(`<span class="slider__step-active"></span>`);
+      } else {
+        sliders.push(`<span></span>`);
+      }
+    }
+    sliderSteps.innerHTML = [...sliders].join("");
+  }
+  onClick = (event) => {
+    let slider = this.elem.querySelector(".slider");
+    let thumb = slider.querySelector(".slider__thumb"); // Ползунок
+    let progress = slider.querySelector(".slider__progress"); // Бар для результата
+    let left = event.clientX - slider.getBoundingClientRect().left;
+    let leftRelative = left / slider.offsetWidth;
+    let segments = this.steps - 1;
+    let approximateValue = leftRelative * segments;
+    let value = Math.round(approximateValue);
+    let valuePercents = (value / segments) * 100;
+    thumb.style.left = `${valuePercents}%`;
+    progress.style.width = `${valuePercents}%`;
+    slider.querySelector(".slider__value").innerHTML = value;
+    this.value = +slider.querySelector(".slider__value").innerHTML;
+
+    let customEvent = new CustomEvent("slider-change", {
+      detail: this.value,
+      bubbles: true,
+    });
+    slider.dispatchEvent(customEvent);
+  };
+
+  addEventListeners() {
+    this.elem.addEventListener("click", this.onClick);
   }
 }
